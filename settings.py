31e2e3582cc38f722026a544276eda01761d2438
stgreen <stgreen@ncsu.edu>
2018-02-22 22:44:37 -0500

    Add settings file

---
+++ settings.py
@@ -0,0 +1,16 @@
+# Course name
+# Can use:
+#  Class code (e.g. CSC 226),
+#  Class name (e.g. Discrete Mathematics for Computer Scientists)
+#  Zybook code (Found in URL when accessing zybook e.g.NCSUCSC226ScafuroSpring2018)
+COURSE_NAME = "CSC 226"
+
+# The chapter in which to complete sections
+CHAPTER_NUMBERS = 1
+
+# The sections to complete (in a comma separated lists
+# Use * for all sections in a chapter
+SECTION_NUMBERS = "1"
+
+# Time between completions (in ms)
+TIME_INTERVAL = 10000
